aed670ebe506546d083c5eb89e6652634df7896f
Mikael Brockman <mikael@brockman.se>
2021-12-12 12:48:18 +0200

    export our git commands

--- common.nix
+++ common.nix
@@ -20,26 +20,7 @@
       fi
     '')
 
-    (pkgs.writeShellScriptBin "git-save" ''
-      git add -A
-      git diff HEAD --quiet || git summary | git commit -F-
-    '')
-
-    (pkgs.writeShellScriptBin "git-summary" ''
-      echo -n "<`whoami`> "
-      if test `git status --short | wc -l` = 1
-      then echo `git status --short` `git shortstat | sed 's,1 file changed ,,'`
-      else git shortstat
-      fi
-    '')
-
-    (pkgs.writeShellScriptBin "git-shortstat" ''
-      git diff HEAD --shortstat | cut -c2- | sed s/,//g |
-      sed -E 's/([0-9]+) insertions?\(\+\)/+\1/' |
-      sed -E 's/([0-9]+) deletions?\(-\)/-\1/' |
-      sed -E 's/\+([0-9]+) -([0-9]+)/+\1\/-\2/' |
-      sed -E 's/([^ ]+)$/(\1)/'
-    '')
+    pkgs.restless-git
   ];
 
   environment.interactiveShellInit = ''
