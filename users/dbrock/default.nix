712e1c4d131fd04b5653cab29d6d9053008fda42
Mikael Brockman <mikael@brockman.se>
2021-12-26 14:21:59 +0000

    <mbrock> M users/dbrock/default.nix (+2/-2)

--- users/dbrock/default.nix
+++ users/dbrock/default.nix
@@ -3,12 +3,12 @@
 let keys = import ../../keys.nix;
 
 in {
-  users.users.root.openssh.authorizedKeys.keys = [keys.dbrock];
+  users.users.root.openssh.authorizedKeys.keys = keys.dbrock;
 
   users.users.dbrock = {
     isNormalUser = true;
     extraGroups = ["wheel" "audio"];
-    openssh.authorizedKeys.keys = [keys.dbrock];
+    openssh.authorizedKeys.keys = keys.dbrock;
   };
 
   home-manager.users.dbrock = import ./profile.nix;
